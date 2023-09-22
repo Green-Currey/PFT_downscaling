bca7ad2de07bee271e6f3c678d94c0e28db1b7ae
Bryce Currey <brycecurrey93@gmail.com>
2023-09-22 18:33:48 -0400

    disco updates

--- execute_LPJ_downscaling_continental.sh
+++ execute_LPJ_downscaling_continental.sh
@@ -3,7 +3,7 @@
 #......................................................................
 #  E D I T:   S L U R M   A R R A Y   J O B    D E T A I L S
 #......................................................................
-#SBATCH --job-name=PFT
+#SBATCH --job-name=PFT_NA
 #SBATCH --time=11:59:00
 #SBATCH --account=s3673
 #SBATCH --mem=180G
@@ -23,14 +23,15 @@ export chunksize=500    #in km2
 export year=2020
 export reflectanceType="DR"
 export version="LPJ_maxfpc_v2.0"
+export continent='North America'
 
-export scriptspath="/discover/nobackup/bcurrey/PFT_downscaling2/scripts/"
-export inpath="/discover/nobackup/bcurrey/PFT_downscaling2/data/"
+export scriptspath="/discover/nobackup/bcurrey/PFT_downscaling/scripts/"
+export inpath="/discover/nobackup/bcurrey/PFT_downscaling/data/"
 export lpjpath="/discover/nobackup/projects/SBG-DO/bcurrey/global_run_simulations/${rundir}/ncdf_outputs/"
-export outpath="/discover/nobackup/projects/SBG-DO/bcurrey/PFT_downscaling2/outputs/"
-export outname="LPJ_prosail_levelC_${reflectanceType}_Version022_1km_m_${year}.nc"
+export outpath="/discover/nobackup/projects/SBG-DO/bcurrey/PFT_downscaling/outputs/"
+export outname="LPJ_prosail_levelC_${reflectanceType}_Version022_1km_m_${year}_${continent}.nc"
 
 
-Rscript $scriptspath/LPJ_PFT_downscaling2.R
+Rscript $scriptspath/LPJ_PFT_downscaling_continental.R
 
 
